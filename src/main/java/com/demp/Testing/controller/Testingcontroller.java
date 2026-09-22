a5cc7b5b58d047ebaef9bf0490a31f0d30525c85
unknown <as2085582@gmail.com>
2026-09-22 19:13:28 +0530

    first commit

---
+++ src/main/java/com/demp/Testing/controller/Testingcontroller.java
@@ -0,0 +1,16 @@
+package com.demp.Testing.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/find")
+public class Testingcontroller {
+
+	@GetMapping("name")
+	public String getName() {
+		return "Hello Amit";
+	}
+
+}
